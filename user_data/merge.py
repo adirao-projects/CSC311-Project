00c2ea6ec97b21aae9766b74feba5897dd421496
Adi Rao <adityakrao.projects@gmail.com>
2024-08-09 11:22:46 -0400

    updated user-data-gpt

---
+++ user_data/merge.py
@@ -0,0 +1,23 @@
+import numpy as np
+uids =  ["71845", "6124931", "3817598", "5288646", "18350315"]
+
+merged = {}
+
+for u in uids:
+    merged[u] = {}
+    for t in np.arange(0,2.5,0.5):
+        merged[u][t] = []
+        for i in range(1,5):
+            if i == 1:
+                i= ""
+            else:
+                i = f"_v{i}"
+            try:
+                f = open(f'{u}_real_GPT{i}_{t}.txt', 'r').read().strip('"').strip("'").split('/n')
+                merged[u][t]+=f[:]
+            except Exception as e:
+                print(e)
+            
+        f = open(f"{u}_real_GPT_sum_{t}", 'w')
+        f.write("\n".join(merged[u][t]))
+        f.close()
